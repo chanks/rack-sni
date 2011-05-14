3dec5ac3eb226708947d3cafdabd173da582965b
Chris Hanks <christopher.m.hanks@gmail.com>
2011-05-13 18:04:44 -0700

    Remove unnecessary stuff from spec_helper.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,11 +1,3 @@
 require 'rack-sni'
 require 'rack/test'
-
 require 'ruby-debug'
-
-# Requires supporting files with custom matchers and macros, etc,
-# in ./support/ and its subdirectories.
-Dir["#{File.dirname(__FILE__)}/support/**/*.rb"].each { |file| require file }
-
-# Require shared example groups before running any specs
-Dir["#{File.dirname(__FILE__)}/**/shared/*.rb"].each { |file| require file }
