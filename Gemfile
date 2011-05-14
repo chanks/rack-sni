5a077e9586648cac0518097e3f62a46ca1d33f19
Chris Hanks <christopher.m.hanks@gmail.com>
2011-05-13 17:50:04 -0700

    Make ruby-debug available in specs.

--- Gemfile
+++ Gemfile
@@ -2,3 +2,9 @@ source "http://rubygems.org"
 
 # Specify your gem's dependencies in rack-sni.gemspec
 gemspec
+
+if RUBY_VERSION < '1.9'
+  gem 'ruby-debug'
+else
+  gem 'ruby-debug19'
+end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,8 @@
 require 'rack-sni'
 require 'rack/test'
 
+require 'ruby-debug'
+
 # Requires supporting files with custom matchers and macros, etc,
 # in ./support/ and its subdirectories.
 Dir["#{File.dirname(__FILE__)}/support/**/*.rb"].each { |file| require file }
